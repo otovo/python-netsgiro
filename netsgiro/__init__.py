27063cd0dd6c3734cdd6fb4137da29528cb3baef
Stein Magnus Jodal <jodal@otovo.no>
2017-02-23 01:04:20 +0100

    Parse transmission records

--- netsgiro/__init__.py
+++ netsgiro/__init__.py
@@ -1,1 +1,99 @@
+import re
+from typing import List
+
+import attr
+
+
 __version__ = '0.1.0'
+
+
+@attr.s
+class Transmission:
+    start_record = attr.ib()
+    end_record = attr.ib()
+
+    @classmethod
+    def from_ocr(cls, lines: List[str]) -> 'Transmission':
+        if len(lines) < 2:
+            raise ValueError('Transmission must have at least two lines')
+
+        for line in lines:
+            if len(line) != 80:
+                raise ValueError('All lines must be exactly 80 chars long')
+
+        start_line, lines, end_line = lines[0], lines[1:-1], lines[-1]
+
+        start_record = TransmissionStart.from_string(start_line)
+        end_record = TransmissionEnd.from_string(end_line)
+
+        return Transmission(
+            start_record=start_record,
+            end_record=end_record,
+        )
+
+
+@attr.s
+class Record:
+    service_code = attr.ib()
+    transmission_type = attr.ib()
+    record_type = attr.ib()
+
+    _PATTERN = re.compile(r'^NY.{78}$')
+
+    @classmethod
+    def from_string(cls, line: str) -> 'Record':
+        matches = re.match(cls._PATTERN, line)
+        if matches is None:
+            raise ValueError('Data did not match data format')
+        return cls(**matches.groupdict())
+
+
+@attr.s
+class TransmissionStart(Record):
+    data_transmitter = attr.ib()
+    transmission_number = attr.ib()
+    data_recipient = attr.ib()
+
+    _PATTERN = re.compile(r'''
+        ^
+        NY      # Format code
+        (?P<service_code>00)
+        (?P<transmission_type>00)
+        (?P<record_type>10)
+
+        (?P<data_transmitter>\d{8})
+        (?P<transmission_number>\d{7})
+        (?P<data_recipient>\d{8})
+
+        0{49}   # Padding
+        $
+    ''', re.VERBOSE)
+
+
+@attr.s
+class TransmissionEnd(Record):
+    num_transactions = attr.ib()
+    num_records = attr.ib()
+    total_amount = attr.ib()
+    nets_date = attr.ib()
+
+    _PATTERN = re.compile(r'''
+        ^
+        NY      # Format code
+        (?P<service_code>00)
+        (?P<transmission_type>00)
+        (?P<record_type>89)
+
+        (?P<num_transactions>\d{8})
+        (?P<num_records>\d{8})
+        (?P<total_amount>\d{17})
+        (?P<nets_date>\d{6})
+
+        0{33}   # Filler
+        $
+    ''', re.VERBOSE)
+
+
+def parse(data: str) -> Transmission:
+    lines = data.strip().splitlines()
+    return Transmission.from_ocr(lines)
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,2 +1,85 @@
-def test_parse():
-    pass
+from pathlib import Path
+
+import pytest
+
+import netsgiro
+
+
+TEST_DIR = Path(__file__).parent
+
+
+@pytest.fixture
+def payment_request_data():
+    filepath = TEST_DIR / 'data' / 'avtalegiro_payment_request.txt'
+    with filepath.open('r', encoding='iso-8859-1') as fh:
+        return fh.read()
+
+
+def test_transmission_with_invalid_start_record():
+    with pytest.raises(ValueError) as exc_info:
+        netsgiro.Transmission.from_ocr([
+            'XX' + ('0' * 78),
+            'NY00008900000006000000220000000000000060'
+            '0170604000000000000000000000000000000000',
+        ])
+
+    assert 'not match data format' in str(exc_info)
+
+
+def test_with_invalid_end_record():
+    with pytest.raises(ValueError) as exc_info:
+        netsgiro.Transmission.from_ocr([
+            'NY00001055555555100008100008080000000000'
+            '0000000000000000000000000000000000000000',
+            'XX' + ('0' * 78),
+        ])
+
+    assert 'not match data format' in str(exc_info)
+
+
+def test_empty_transmission():
+    transmission = netsgiro.Transmission.from_ocr([
+        'NY00001055555555100008100008080000000000'
+        '0000000000000000000000000000000000000000',
+        'NY00008900000006000000220000000000000060'
+        '0170604000000000000000000000000000000000',
+    ])
+
+    assert isinstance(transmission, netsgiro.Transmission)
+
+    assert transmission.start_record.service_code == '00'
+    assert transmission.start_record.transmission_type == '00'
+    assert transmission.start_record.record_type == '10'
+    assert transmission.start_record.data_transmitter == '55555555'
+    assert transmission.start_record.transmission_number == '1000081'
+    assert transmission.start_record.data_recipient == '00008080'
+
+    assert transmission.end_record.service_code == '00'
+    assert transmission.end_record.transmission_type == '00'
+    assert transmission.end_record.num_transactions == '00000006'
+    assert transmission.end_record.num_records == '00000022'
+    assert transmission.end_record.total_amount == '00000000000000600'
+    assert transmission.end_record.nets_date == '170604'
+
+
+def test_parse_empty_string_fails():
+    with pytest.raises(ValueError) as exc_info:
+        netsgiro.parse('')
+
+    assert 'at least two lines' in str(exc_info)
+
+
+def test_parse_too_short_lines_fails():
+    with pytest.raises(ValueError) as exc_info:
+        netsgiro.parse(
+            'NY0000\n'
+            'NY0000\n'
+        )
+
+    assert 'exactly 80 chars long' in str(exc_info)
+
+
+def test_parse_payment_request(payment_request_data):
+    transmission = netsgiro.parse(payment_request_data)
+
+    assert isinstance(transmission, netsgiro.Transmission)
