cc930c0ae0b49f6b063a90664caf1f184e9bce1d
Stein Magnus Jodal <jodal@otovo.no>
2017-02-23 11:44:24 +0100

    Parse AvtaleGiro transaction records

--- tests/test_records.py
+++ tests/test_records.py
@@ -63,3 +63,50 @@ def test_assignment_end():
     assert record.nets_date == '170604'
     assert record.nets_date_earliest == '170604'
     assert record.nets_date_latest == '000000'
+
+
+def test_avtalegiro_amount_item_1():
+    record = records.AvtaleGiroAmountItem1.from_string(
+        'NY2121300000001170604           00000000'
+        '000000100          008000011688373000000'
+    )
+
+    assert record.service_code == '21'
+    assert record.transaction_type == '21'
+    assert record.record_type == '30'
+
+    assert record.transaction_number == '0000001'
+    assert record.due_date == '170604'
+    assert record.amount == '00000000000000100'
+    assert record.kid == '          008000011688373'
+
+
+def test_avtalegiro_amount_item_2():
+    record = records.AvtaleGiroAmountItem2.from_string(
+        'NY2121310000001NAVN                     '
+        '                                   00000'
+    )
+
+    assert record.service_code == '21'
+    assert record.transaction_type == '21'
+    assert record.record_type == '31'
+
+    assert record.transaction_number == '0000001'
+    assert record.payer_name == 'NAVN      '
+    assert record.reference == ' ' * 25
+
+
+def test_avtalegiro_specification():
+    record = records.AvtaleGiroSpecification.from_string(
+        'NY212149000000140011 Gjelder Faktura: 16'
+        '8837  Dato: 19/03/0400000000000000000000'
+    )
+
+    assert record.service_code == '21'
+    assert record.transaction_type == '21'
+    assert record.record_type == '49'
+
+    assert record.transaction_number == '0000001'
+    assert record.line_number == '001'
+    assert record.column_number == '1'
+    assert record.text == ' Gjelder Faktura: 168837  Dato: 19/03/04'
