466736a651952dec7b46ba8b28c724a233395734
Stein Magnus Jodal <jodal@otovo.no>
2017-02-26 00:40:59 +0100

    Add OCR Giro test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,10 @@ def payment_request_data():
     filepath = TEST_DIR / 'data' / 'avtalegiro_payment_request.txt'
     with filepath.open('r', encoding='iso-8859-1') as fh:
         return fh.read()
+
+
+@pytest.fixture
+def ocr_giro_transactions_data():
+    filepath = TEST_DIR / 'data' / 'ocr_giro_transactions.txt'
+    with filepath.open('r', encoding='iso-8859-1') as fh:
+        return fh.read()
