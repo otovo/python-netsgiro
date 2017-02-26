39c3b49f911f6353377dd7e7c359d1e2b05a20a6
Stein Magnus Jodal <jodal@otovo.no>
2017-02-26 01:52:16 +0100

    Parse OCR Giro transactions

--- tests/test_records.py
+++ tests/test_records.py
@@ -101,6 +101,23 @@ def test_assignment_start_for_avtalegiro_cancelation():
     assert record.assignment_account == '88888888888'
 
 
+def test_assignment_start_for_ocr_giro_transactions():
+    record = netsgiro.AssignmentStart.from_string(
+        'NY09002000100856600000029999104276400000'
+        '0000000000000000000000000000000000000000'
+    )
+
+    assert record.service_code == netsgiro.ServiceCode.OCR_GIRO
+    assert record.record_type == netsgiro.RecordType.ASSIGNMENT_START
+
+    assert record.assignment_type == (
+        netsgiro.AvtaleGiroAssignmentType.PAYMENT_REQUESTS)  # TODO Rename
+
+    assert record.agreement_id == '001008566'
+    assert record.assignment_number == '0000002'
+    assert record.assignment_account == '99991042764'
+
+
 def test_assignment_end_for_avtalegiro_payment_requests():
     record = netsgiro.AssignmentEnd.from_string(
         'NY21008800000006000000200000000000000060'
@@ -161,6 +178,26 @@ def test_assignment_end_for_avtalegiro_cancelations():
     assert record.nets_date_latest is None
 
 
+def test_assignment_end_for_ocr_giro_transactions():
+    record = netsgiro.AssignmentEnd.from_string(
+        'NY09008800000020000000420000000000514490'
+        '0200192200192200192000000000000000000000'
+    )
+
+    assert record.service_code == netsgiro.ServiceCode.OCR_GIRO
+    assert record.record_type == netsgiro.RecordType.ASSIGNMENT_END
+
+    assert record.assignment_type == (
+        netsgiro.AvtaleGiroAssignmentType.PAYMENT_REQUESTS)  # TODO: Rename
+
+    assert record.num_transactions == 20
+    assert record.num_records == 42
+    assert record.total_amount == 5144900
+    assert record.nets_date == date(1992, 1, 20)
+    assert record.nets_date_earliest == date(1992, 1, 20)
+    assert record.nets_date_latest == date(1992, 1, 20)
+
+
 def test_transaction_amount_item_1_for_avtalegiro_payment_request():
     record = netsgiro.TransactionAmountItem1.from_string(
         'NY2121300000001170604           00000000'
@@ -197,6 +234,31 @@ def test_transaction_amount_item_1_for_avtalegiro_cancelation():
     assert record.kid == '008000011688373'
 
 
+def test_transaction_amount_item_1_for_ocr_giro_transactions():
+    record = netsgiro.TransactionAmountItem1.from_string(
+        'NY09103000000012001921320101464000000000'
+        '000102000                  0000531000000'
+    )
+
+    assert record.service_code == netsgiro.ServiceCode.OCR_GIRO
+    assert record.record_type == netsgiro.RecordType.TRANSACTION_AMOUNT_1
+
+    assert record.transaction_type == (
+        netsgiro.TransactionType.FROM_GIRO_DEBITED_ACCOUNT)
+    assert record.transaction_number == '0000001'
+
+    assert record.nets_date == date(1992, 1, 20)
+
+    assert record.centre_id == '13'
+    assert record.day_code == 20
+    assert record.partial_settlement_number == 1
+    assert record.partial_settlement_serial_number == '01464'
+    assert record.sign == '0'  # TODO Change type?
+
+    assert record.amount == 102000
+    assert record.kid == '0000531'
+
+
 def test_transaction_amount_item_2_for_avtalegiro_payment_request():
     record = netsgiro.TransactionAmountItem2.from_string(
         'NY2121310000001NAVN                     '
@@ -212,6 +274,42 @@ def test_transaction_amount_item_2_for_avtalegiro_payment_request():
 
     assert record.payer_name == 'NAVN'
     assert record.reference is None
+
+
+def test_transaction_amount_item_2_for_ocr_giro_transactions():
+    record = netsgiro.TransactionAmountItem2.from_string(
+        'NY09103100000019636827194099038562000000'
+        '0160192999905123410000000000000000000000'
+    )
+
+    assert record.service_code == netsgiro.ServiceCode.OCR_GIRO
+    assert record.record_type == netsgiro.RecordType.TRANSACTION_AMOUNT_2
+
+    assert record.transaction_type == (
+        netsgiro.TransactionType.FROM_GIRO_DEBITED_ACCOUNT)
+    assert record.transaction_number == '0000001'
+
+    assert record.form_number == '9636827194'
+    assert record.payer_name is None
+    assert record.reference == '099038562'
+    assert record.bank_date == date(1992, 1, 16)
+    assert record.debit_account == '99990512341'
+
+
+def test_transaction_amount_item_3_for_ocr_giro_transactions():
+    record = netsgiro.TransactionAmountItem3.from_string(
+        'NY0921320000001Foo bar baz              '
+        '               0000000000000000000000000'
+    )
+
+    assert record.service_code == netsgiro.ServiceCode.OCR_GIRO
+    assert record.record_type == netsgiro.RecordType.TRANSACTION_AMOUNT_3
+
+    assert record.transaction_type == (
+        netsgiro.TransactionType.PURCHASE_WITH_TEXT)
+    assert record.transaction_number == '0000001'
+
+    assert record.text == 'Foo bar baz'
 
 
 def test_transaction_specification_for_avtalegiro_payment_request():
