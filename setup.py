1a5f5d215ab7459909491ffc938145acf5b878ea
Kristian Klette <klette@otovo.com>
2019-11-06 16:30:54 +0100

    setup: Add Python 3.8 pypi classifier

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='avtalegiro ocr giro',
     packages=find_packages(exclude=['tests', 'tests.*']),
