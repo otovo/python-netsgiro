8f04ce4ee6b3383bc335f889455705ed7fd1e9b1
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-04-01 14:00:33 +0200

    chore: Replace noqa statements with `per-file-ignore` declaration

--- netsgiro/__init__.py
+++ netsgiro/__init__.py
@@ -7,8 +7,10 @@ __version__ = '1.3.0'
 from netsgiro.constants import *
 from netsgiro.enums import *
 from netsgiro.objects import *
+from netsgiro.utils import *
 
-from netsgiro import constants, enums, objects  # isort: skip
+# Must be placed last
+from netsgiro import constants, enums, objects  # noqa, isort: skip
 
 
-__all__ = constants.__all__ + enums.__all__ + objects.__all__
+__all__ = constants.__all__ + enums.__all__ + objects.__all__ + utils.__all__
