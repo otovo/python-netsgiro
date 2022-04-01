5570c1ab68d0319670b5884cfdc4f6c2b37511f4
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-04-01 15:21:21 +0200

    refactor: Make validation opt-in

--- netsgiro/utils.py
+++ netsgiro/utils.py
@@ -1,12 +1,6 @@
 from contextlib import suppress
 from datetime import date, datetime, timedelta
-from typing import NoReturn
-from typing import TYPE_CHECKING
 from zoneinfo import ZoneInfo
-
-if TYPE_CHECKING:
-    from attr import Attribute
-    from netsgiro import PaymentRequest
 
 __all__ = ['get_minimum_due_date']
 
@@ -14,38 +8,22 @@ __all__ = ['get_minimum_due_date']
 OSLO_TZ = ZoneInfo('Europe/Oslo')
 
 
-def get_minimum_due_date() -> date:
+def get_minimum_due_date(now: datetime) -> date:
     """Return the minimum viable due date for a an ocrgiro created right now."""
-    now = datetime.now(tz=OSLO_TZ)
+    today = now.date()
 
     # 14:00 is the cut-off for sending in new transmissions;
     # files sent after 14:00 are processed the following day.
-    delta = (
-        4 if now.time() < datetime.strptime('02:00PM', '%I:%M%p').time() else 5
-    )
+    delta = 5 if now.hour > 13 else 4
 
     # Adjust for holidays, if the client has the optional dependency installed
     with suppress(ImportError):
         from holidays import country_holidays
 
-        norwegian_holidays = country_holidays('NO')
-        number_of_holidays = len(
-            norwegian_holidays[
-            now.strftime('%x'): (now + timedelta(days=delta)).strftime(
-                '%x'
-            )
-            ]
-        )
-        delta += number_of_holidays
+        delta += len(country_holidays('NO')[now : now + timedelta(days=delta)])
+
+    # File have to be received on weekdays to be processed the same day
+    if today.weekday() in [5, 6]:
+        delta += 7 - today.weekday()
 
     return (now + timedelta(days=delta)).date()
-
-
-def validate_minimum_date(inst: 'PaymentRequest', attr: 'Attribute', value: date) -> NoReturn:
-    """Make sure payment request dates are gt the minimum allowed date."""
-    if value < get_minimum_due_date():
-        raise ValueError(
-            'The minimum due date of a transaction is today + 4 calendar days.'
-            ' OCR files with due dates earlier than this will be rejected when'
-            ' submitted.'
-        )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,62 +1,69 @@
 from datetime import datetime, timedelta
 
 import holidays
-import time_machine
 
 from netsgiro.utils import OSLO_TZ, get_minimum_due_date
 
-today = datetime(2022, 4, 1, 13, 59, tzinfo=OSLO_TZ)
-today_after_cutoff = datetime(2022, 4, 1, 14, 1, tzinfo=OSLO_TZ)
-day_before_easter = datetime(2022, 4, 13, 13, 59, tzinfo=OSLO_TZ)
-day_before_easter_after_cutoff = datetime(2022, 4, 13, 23, 59, tzinfo=OSLO_TZ)
+monday = datetime(2022, 3, 28, 13, 59, tzinfo=OSLO_TZ)
 
 
-@time_machine.travel(today)
 def test_minimum_due_date_before_cutoff():
     """
     Files generated before 14:00 Norwegian time should be
     adjusted by 4 days, minimum.
     """
-    assert get_minimum_due_date() == today + timedelta(days=4)
+    assert get_minimum_due_date(monday) == (monday + timedelta(days=4)).date()
 
 
-@time_machine.travel(today_after_cutoff)
+monday_after_cutoff = datetime(2022, 4, 1, 14, 1, tzinfo=OSLO_TZ)
+
+
 def test_minimum_due_date_after_cutoff():
     """
     Because files sent in after 14:00 are processed the next day, files
-    generated after 14:00 Norwegian time should be adjusted by 5 days, minimum.
-
+    generated after 14:00 Norwegian time should be adjusted by 5 days.
     """
     assert (
-        get_minimum_due_date()
-        == (today_after_cutoff + timedelta(days=5)).date()
+        get_minimum_due_date(monday_after_cutoff)
+        == (monday_after_cutoff + timedelta(days=5)).date()
     )
 
 
-@time_machine.travel(day_before_easter)
+day_before_easter = datetime(2022, 4, 13, 13, 59, tzinfo=OSLO_TZ)
+
+
 def test_minimum_due_date_with_holidays_before_cutoff():
     """
     There are 2 holidays in the span 13-17th of April 2022,
     so we expect the function to adjust the timedelta by 2 days.
     """
     assert (
-        get_minimum_due_date() == (day_before_easter + timedelta(days=6)).date()
+        get_minimum_due_date(day_before_easter)
+        == (day_before_easter + timedelta(days=6)).date()
     )
 
 
-@time_machine.travel(day_before_easter_after_cutoff)
+day_before_easter_after_cutoff = datetime(2022, 4, 13, 23, 59, tzinfo=OSLO_TZ)
+
+
 def test_minimum_due_date_with_holidays_after_cutoff():
     """
     If we send in the file after the cut-off, number of
     holidays is upped to 3, so we expect 2 more days of offsetting.
     """
     assert (
-        get_minimum_due_date()
+        get_minimum_due_date(day_before_easter_after_cutoff)
         == (day_before_easter_after_cutoff + timedelta(days=8)).date()
     )
 
 
-@time_machine.travel(today)
+friday = datetime(2022, 4, 1, 23, 59, tzinfo=OSLO_TZ)
+
+
+def test_minimum_due_date_with_now_being_a_saturday():
+    assert get_minimum_due_date(friday) == (friday + timedelta(days=5)).date()
+
+
 def test_minimum_due_date_without_holiday_dependency():
     """
     Make sure an ImportError from a missing dependency isn't propagated.
@@ -64,5 +71,5 @@ def test_minimum_due_date_without_holiday_dependency():
     import sys
 
     sys.modules['holidays'] = None
-    assert get_minimum_due_date() == (today + timedelta(days=4)).date()
+    assert get_minimum_due_date(monday) == (monday + timedelta(days=4)).date()
     sys.modules['holidays'] = holidays
