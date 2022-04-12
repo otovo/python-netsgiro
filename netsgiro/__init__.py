eb7968fd3046c757d811a128653a235f4f201262
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-04-12 21:12:06 +0200

    chore: Remove excess imports
    It makes sense to me that re-exporting the
    __all__ declarations is all we really want
    to do. This should be the definition of our
    public API.

--- netsgiro/__init__.py
+++ netsgiro/__init__.py
@@ -1,14 +1,7 @@
 """File parsers for Nets AvtaleGiro and OCR Giro files."""
 
+from netsgiro import constants, enums, objects
 
 __version__ = '1.3.0'
-
-
-from netsgiro.constants import *
-from netsgiro.enums import *
-from netsgiro.objects import *
-
-from netsgiro import constants, enums, objects  # isort: skip
-
 
 __all__ = constants.__all__ + enums.__all__ + objects.__all__
