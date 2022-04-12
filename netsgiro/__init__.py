a0591844a1e7ba38339e8ff4531546ba4024e388
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-04-12 21:01:51 +0200

    chore: Replace `noqa` statements with per-file-ignore setting

--- netsgiro/__init__.py
+++ netsgiro/__init__.py
@@ -4,12 +4,11 @@
 __version__ = '1.3.0'
 
 
-from netsgiro.constants import *  # noqa: Reexport
-from netsgiro.enums import *  # noqa: Reexport
-from netsgiro.objects import *  # noqa: Reexport
+from netsgiro.constants import *
+from netsgiro.enums import *
+from netsgiro.objects import *
 
-
-from netsgiro import constants, enums, objects  # noqa: Must come after reexport
+from netsgiro import constants, enums, objects  # isort: skip
 
 
 __all__ = constants.__all__ + enums.__all__ + objects.__all__
